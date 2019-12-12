3630b7898c0d14aa36699d0982038fa28ea8a3c3
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2019-12-12 20:19:57 +1100

    cmd/spr-dump: initial commit

---
+++ cmd/spr-dump/README.md
@@ -0,0 +1,42 @@
+# spr-dump
+
+A program that reads through every `.SPR` sprite file in Dark Omen's data and dumps out each of the sprite's frames as PNG images.
+
+## Installation
+
+Use `go install` to download and install the program
+
+```shell
+go install github.com/jonathaningram/dark-omen/cmd/spr-dump
+```
+
+See `go help install` for more information.
+
+## Usage
+
+Pass the path to the Dark Omen CD data as well as the path to the output directory.
+
+```shell
+spr-dump -dark-omen-path=/dark-omen-game-from-cd -output-path=/tmp/dark-omen-spr-dump
+```
+
+For Windows the command may look something like:
+
+```shell
+spr-dump.exe -dark-omen-path=D:\ -output-path=C:\tmp\dark-omen-spr-dump
+```
+
+The output will look something like this (for a sprite file named `BERNHD.SPR`):
+
+```shell
+$ ls -l /tmp/dark-omen-spr-dump/DARKOMEN/DARKOMEN/GRAPHICS/SPRITES/BERNHD.SPR/
+0.png
+1.png
+2.png
+3.png
+...
+100.png
+101.png
+102.png
+103.png
+```
---
+++ cmd/spr-dump/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image/png"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/jonathaningram/dark-omen/encoding/spr"
+)
+
+func main() {
+	const (
+		flagDarkOmenPath = "dark-omen-path"
+		flagOutputPath   = "output-path"
+	)
+
+	var (
+		darkOmenPath = flag.String(flagDarkOmenPath, "", "path to Dark Omen CD data")
+		outputPath   = flag.String(flagOutputPath, "", "path to directory in which sprites will be dumped")
+	)
+
+	flag.Parse()
+
+	if *darkOmenPath == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *outputPath == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	err := filepath.Walk(*darkOmenPath, func(p string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if path.Ext(info.Name()) != ".SPR" {
+			return nil
+		}
+
+		f, err := os.Open(p)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		relativePath := strings.TrimPrefix(p, *darkOmenPath)
+
+		fmt.Printf("Extracting %s...", relativePath)
+
+		d := spr.NewDecoder(f)
+
+		sprite, err := d.Decode()
+		if err != nil {
+			fmt.Printf("failed\n")
+			return fmt.Errorf("could not decode %s: %w", relativePath, err)
+		}
+
+		fmt.Printf("ok\n")
+
+		dir := path.Join(*outputPath, relativePath)
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			return err
+		}
+
+		fmt.Printf("Creating %d sprite(s) for %s...", len(sprite.Frames), relativePath)
+
+		for i, f := range sprite.Frames {
+			if f.Type == spr.FrameTypeEmpty {
+				continue
+			}
+
+			file := path.Join(dir, fmt.Sprintf("%d.png", i))
+			out, err := os.Create(file)
+			if err != nil {
+				fmt.Printf("failed\n")
+				return err
+			}
+
+			err = png.Encode(out, f.Image)
+			if err != nil {
+				fmt.Printf("failed\n")
+				return fmt.Errorf("could not encode PNG file for frame %d: %w", i, err)
+			}
+		}
+
+		fmt.Printf("ok\n")
+
+		return nil
+	})
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
