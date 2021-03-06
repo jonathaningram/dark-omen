5c802e525ee6673bef9c90a965bd2174a0f7733a
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2021-03-06 16:13:04 +1100

    encoding/fnt: initial commit
    It partially works, but there are some unknowns and TODOs in the
    decoding, related to heights and line heights.
    
    It is missing the encoding functions that allow for round trip testing.
    
    It is also missing most tests compared to the other encoding packages.

---
+++ encoding/fnt/fnt_test.go
@@ -0,0 +1,89 @@
+package fnt
+
+import (
+	"bytes"
+	"image"
+	"io/ioutil"
+	"path"
+	"testing"
+)
+
+var font *Font
+
+func BenchmarkDecode(b *testing.B) {
+	tests := []string{
+		"F_MENBG.FNT",
+		"F_HELP.FNT",
+	}
+	for _, tt := range tests {
+		b.Run(tt, func(b *testing.B) {
+			b.ReportAllocs()
+			bs, err := ioutil.ReadFile(path.Join("testdata", tt))
+			if err != nil {
+				b.Fatal(err)
+			}
+			r := bytes.NewReader(bs)
+			var f *Font
+			for n := 0; n < b.N; n++ {
+				b.StopTimer()
+				r.Reset(bs)
+				d := NewDecoder(r)
+				b.StartTimer()
+				var err error
+				f, err = d.Decode()
+				if err != nil {
+					b.Fatalf("Decode() error = %v, want nil", err)
+				}
+			}
+			font = f
+		})
+	}
+}
+
+func Test_xy(t *testing.T) {
+	type args struct {
+		img *image.NRGBA
+		x   int
+		y   int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wantX int
+		wantY int
+	}{
+		{
+			name: "2x2 image first row",
+			args: args{
+				image.NewNRGBA(image.Rect(0, 0, 2, 2)),
+				0, 0,
+			},
+			wantX: 1,
+			wantY: 0,
+		},
+		{
+			name: "2x2 image second row",
+			args: args{
+				image.NewNRGBA(image.Rect(0, 0, 2, 2)),
+				1, 0,
+			},
+			wantX: 0,
+			wantY: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotX, gotY := xy(tt.args.img, tt.args.x, tt.args.y)
+			if !(gotX == tt.wantX && gotY == tt.wantY) {
+				t.Errorf("xy() = (%v,%v), want (%v,%v)", gotX, gotY, tt.wantX, tt.wantY)
+			}
+		})
+	}
+}
+
+func truncateBytes(bs []byte, size int) []byte {
+	if len(bs) > size {
+		return bs[:size]
+	}
+	return bs
+}
